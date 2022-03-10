ad12e2031ae151f811fd9edaedba35882816e32f
Abdon <estadia2020@gmail.com>
2022-03-09 21:02:50 -0600

    home and input page

--- src/pages/InputPage/InputPage.jsx
+++ src/pages/InputPage/InputPage.jsx
@@ -2,36 +2,16 @@ import React from "react";
 import styles from "./InputPage.module.css";
 import { useGetTLOSAccount } from "../../hooks/useGetTLOSAccount";
 //----Componentes del input
-import { alpha, styled } from "@mui/material/styles";
 import Box from "@mui/material/Box";
 import TextField from "@mui/material/TextField";
 //---Componentes del botón
+import ButtonGroup from "@mui/material/ButtonGroup";
 import Button from "@mui/material/Button";
 import SendIcon from "@mui/icons-material/Send";
 import Stack from "@mui/material/Stack";
 //--Componetes de la carta y gird
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Grid";
-
-const CssTextField = styled(TextField)({
-  "& label.Mui-focused": {
-    color: "green",
-  },
-  "& .MuiInput-underline:after": {
-    borderBottomColor: "green",
-  },
-  "& .MuiOutlinedInput-root": {
-    "& fieldset": {
-      borderColor: "blue",
-    },
-    "&:hover fieldset": {
-      borderColor: "yellow",
-    },
-    "&.Mui-focused fieldset": {
-      borderColor: "green",
-    },
-  },
-});
 
 export const InputPage = () => {
   const { data, accountName, loadingAccount, setAccountName, letsSearch } =
@@ -41,7 +21,7 @@ export const InputPage = () => {
     <main className={styles.lienzo}>
       <Grid container spacing={1}>
         <Grid item sm={1} md={1} lg={1}></Grid>
-        <Grid item xs={12} sm={10} md={10} lg={10}>
+        <Grid item xs={12} sm={10} md={10} lg={10} className={styles.li}>
           <Box
             component="form"
             noValidate
@@ -51,34 +31,34 @@ export const InputPage = () => {
               gap: 2,
             }}
           >
-            <CssTextField
-              label="Custom CSS"
-              id="custom-css-outlined-input"
-              type="text"
-              onChange={(e) => setAccountName(e.target.value)}
-              value={accountName}
-            />
+            <ButtonGroup disableElevation variant="contained">
+              <TextField
+                id="outlined-basic"
+                label="Outlined"
+                variant="outlined"
+                className={styles.campoTXT}
+                onChange={(e) => setAccountName(e.target.value)}
+                value={accountName}
+              />
 
-            <Stack direction="row" spacing={1}>
-              <Button
-                variant="contained"
-                endIcon={<SendIcon />}
-                onClick={letsSearch}
-              >
-                Next
-              </Button>
-            </Stack>
-
-            <Typography
-              sx={{ fontSize: 16 }}
-              color="text.secondary"
-              gutterBottom
-            >
-              {loadingAccount === false
-                ? data.account.account_name
-                : "loading..."}
-            </Typography>
+              <Stack direction="row" spacing={1}>
+                <Button
+                  variant="contained"
+                  endIcon={<SendIcon />}
+                  onClick={letsSearch}
+                  className={styles.boton}
+                >
+                  Next
+                </Button>
+              </Stack>
+            </ButtonGroup>
           </Box>
+
+          <Typography sx={{ fontSize: 16 }} color="text.secondary" gutterBottom>
+            {loadingAccount === false
+              ? data.account.account_name
+              : "loading..."}
+          </Typography>
         </Grid>
         <Grid item sm={1} md={1} lg={1}></Grid>
       </Grid>
