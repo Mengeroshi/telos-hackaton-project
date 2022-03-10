f58cf281908ccb18af11100e4daa37bfc73a9cf1
Abdon <estadia2020@gmail.com>
2022-03-09 20:21:54 -0600

    prototype homePage

--- src/pages/Home/Grafica/Grafico.jsx
+++ src/pages/Home/Grafica/Grafico.jsx
@@ -1,103 +1,12 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
-import {
-  Chart,
-  ArgumentAxis,
-  ValueAxis,
-  LineSeries,
-  Title,
-  Legend,
-} from '@devexpress/dx-react-chart-material-ui';
-import { styled } from '@mui/material/styles';
-import { Animation } from '@devexpress/dx-react-chart';
+import React, { Component } from "react";
+import styles from "./Grafico.module.css";
 
-import { confidence as data } from './data-vizualization.js';
-
-const PREFIX = 'Demo';
-
-const classes = {
-  chart: `${PREFIX}-chart`,
-};
-
-const format = () => tick => tick;
-
-const Root = props => (
-  <Legend.Root {...props} sx={{ display: 'flex', margin: 'auto', flexDirection: 'row' }} />
-);
-const Label = props => (
-  <Legend.Label sx={{ pt: 1, whiteSpace: 'nowrap' }} {...props} />
-);
-const Item = props => (
-  <Legend.Item sx={{ flexDirection: 'column' }} {...props} />
-);
-
-const ValueLabel = (props) => {
-  const { text } = props;
-  return (
-    <ValueAxis.Label
-      {...props}
-      text={`${text}%`}
-    />
-  );
-};
-
-const TitleText = props => (
-  <Title.Text {...props} sx={{ whiteSpace: 'pre' }} />
-);
-
-const StyledChart = styled(Chart)(() => ({
-  [`&.${classes.chart}`]: {
-    paddingRight: '20px',
-  },
-}));
-
-export default class Demo extends React.PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      data,
-    };
-  }
-
+export default class Grafico extends Component {
   render() {
-    const { data: chartData } = this.state;
-
     return (
-      <Paper>
-        <StyledChart
-          data={chartData}
-          className={classes.chart}
-        >
-          <ArgumentAxis tickFormat={format} />
-          <ValueAxis
-            max={50}
-            labelComponent={ValueLabel}
-          />
-
-          <LineSeries
-            name="TV news"
-            valueField="tvNews"
-            argumentField="year"
-          />
-          <LineSeries
-            name="Church"
-            valueField="church"
-            argumentField="year"
-          />
-          <LineSeries
-            name="Military"
-            valueField="military"
-            argumentField="year"
-          />
-          <Legend position="bottom" rootComponent={Root} itemComponent={Item} labelComponent={Label} />
-          <Title
-            text={`Confidence in Institutions in American society ${'\n'}(Great deal)`}
-            textComponent={TitleText}
-          />
-          <Animation />
-        </StyledChart>
-      </Paper>
+      <div className="container-fluid">
+          <div className={styles.item}></div>
+      </div>
     );
   }
 }
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -3,29 +3,36 @@ import styles from "./Home.module.css";
 import Grid from "@mui/material/Grid";
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+
 //--
-//import Grafico from './Grafica/Grafico.jsx';
+import Grafico from "./Grafica/Grafico.jsx";
+
+import Navbar from "./Nav/Navbar.jsx";
 import Tablas from "./Table/Tabla.jsx";
 
 export const Home = () => {
   return (
     <main>
-      <div className={styles.Nav}>Navbar</div>
+      <div>
+        <Navbar />
+      </div>
 
       <Container maxWidth="xlg">
-        <Box sx={{ height: "100vh" }}>
+        <Box sx={{ height: "100vh", marginBottom: "300px" }}>
           <Grid container spacing={2}>
             <Grid item xs={12} sm={12} md={12} lg={12} xl={12}>
-              <div className={styles.EstiloPrecio}>Precio</div>
+              <div className={styles.EstiloPrecio}>
+                <p>$1,000,000.00 USD</p>
+              </div>
             </Grid>
 
             <Grid item xs={12} sm={12} md={8} lg={8} xl={8}>
               <div className={styles.EstiloGrafi}>
-                  Contenedor1
-                
+                Contenedor1
+                <Grafico></Grafico>
               </div>
               <div className={styles.EstiloTabla}>
-                  <Tablas/>
+                <Tablas />
               </div>
             </Grid>
 
@@ -35,6 +42,15 @@ export const Home = () => {
           </Grid>
         </Box>
       </Container>
+
+      <Box
+        component="span"
+        sx={{ display: { xs: "none", sm: "none", md: "block" } }}
+      >
+        <footer className={styles.Estilofooter}>
+          <div className={styles.FoooterContent}>Copyright ...</div>
+        </footer>
+      </Box>
     </main>
   );
 };
