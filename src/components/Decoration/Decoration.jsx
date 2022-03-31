d20c4ec2f5ac4307d568b7e6d8dcd4d2c9f20ccf
Abdon <estadia2020@gmail.com>
2022-03-31 13:05:39 -0600

    añadiendo decoracion

---
+++ src/components/Decoration/Decoration.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Decoration.module.css";
+
+export default function Decoration() {
+  return (
+    <div>
+      <div className={styles.cuadro1}></div>
+      <div className={styles.cuadro2}></div>
+      <div className={styles.cuadro3}></div>
+      <div className={styles.cuadro4}></div>
+      <div className={styles.cuadro5}></div>
+    </div>
+  );
+}
--- src/pages/Portfolio/Portfolio.jsx
+++ src/pages/Portfolio/Portfolio.jsx
@@ -11,23 +11,23 @@ import { ContextApp } from "../../context/Context";
 import { TokenItem } from "../../components/TokenItem/TokenItem";
 import { tlosToStart } from "../../utils/tlosToStart";
 import { useFetchTlosPrices } from "../../hooks/useFetchTlosPrices";
+import Decoration from "../../components/Decoration/Decoration.jsx";
 
 export const Portfolio = () => {
   const [state] = React.useContext(ContextApp);
-  const {data, tokens} = state;
+  const { data, tokens } = state;
   let tokensSorted = tlosToStart(tokens) || tokens;
   const { prices, loadingPrices } = useFetchTlosPrices();
-  const isTelosOnWallet =tokensSorted[0].token === "TLOS";
+  const isTelosOnWallet = tokensSorted[0].token === "TLOS";
   console.log(isTelosOnWallet);
 
   const TLOSAmount = tokensSorted[0].amount;
   const lastPrice = prices[prices?.length - 1]?.price || 100;
- 
+
   const netWorth = isTelosOnWallet
     ? (TLOSAmount * lastPrice).toFixed(2)
     : "No available";
 
-  
   return (
     <main>
       <div>
@@ -36,10 +36,21 @@ export const Portfolio = () => {
 
       <Container maxWidth="xlg">
         <div>
-          <Box sx={{ height: "100vh", marginBottom: "300px"}}>
-            <Grid container spacing={2} sx={{ display: "flex", justifyContent: "center" }}>
-
-              <Grid container className={styles.EstiloPrecio} >
+          <Decoration />
+          <Box
+            sx={{
+              height: "100vh",
+              marginBottom: "300px",
+              position: "relative",
+              zIndex: "2",
+            }}
+          >
+            <Grid
+              container
+              spacing={2}
+              sx={{ display: "flex", justifyContent: "center" }}
+            >
+              <Grid container className={styles.EstiloPrecio}>
                 <Grid item xs={10} sm={10} md={10} lg={10} xl={10}>
                   <div>
                     <span>Portfolio</span>
@@ -60,18 +71,18 @@ export const Portfolio = () => {
                     dataPrices={prices}
                     amount={TLOSAmount}
                     lastPrice={lastPrice}
-                   />
+                  />
                 </div>
                 <div className={styles.EstiloTabla}>
                   <h2 className={styles.Titulos}>Historial</h2>
-                  <DataTable/>
+                  <DataTable />
                 </div>
               </Grid>
 
               <Grid item xs={12} sm={12} md={4.05} lg={4} xl={4}>
                 <div className={styles.EstiloTokenGrafi}>
                   <div className={styles.SizeGrafi}>
-                  <h2 className={styles.Titulos}>Assets</h2>
+                    <h2 className={styles.Titulos}>Assets</h2>
                     <DoughnutChart />
                   </div>
                 </div>
@@ -81,18 +92,17 @@ export const Portfolio = () => {
                     <h2 className={styles.Titulos}>Tokens</h2>
                     {/* Loop creado solo para simular un listado de tokens */}
                     <ul className={styles.TokenList}>
-                    {tokensSorted.map((token, i) => {
-                      return(
-                        <TokenItem 
-                          key={i}
-                          ticker={token.token}
-                          amount={token.amount}
-                          gradientList={token.colorList}
-                        />
-                      ) 
-                    })}
+                      {tokensSorted.map((token, i) => {
+                        return (
+                          <TokenItem
+                            key={i}
+                            ticker={token.token}
+                            amount={token.amount}
+                            gradientList={token.colorList}
+                          />
+                        );
+                      })}
                     </ul>
-                    
                   </div>
                 </div>
               </Grid>
