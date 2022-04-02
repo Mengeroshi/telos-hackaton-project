411086a142a53c75f98c4e3baa204a71ffbce8ee
mengeroshi <dpllgr@hotmail.com>
2022-04-02 01:34:15 -0600

    getLaste block with txs

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import { randomGradientAlpha } from "../utils/randomGradientAlpha";
 import { randomPrice } from "../utils/randomPrice";
-import { getERCBalanceList } from "../utils/getERCBalanceList";
 import Web3 from "web3";
-import { getTLOSBalance } from "../utils/getTLOSBalance";
+// import { getTLOSBalance } from "../utils/getTLOSBalance";
+// import { getERCBalanceList } from "../utils/getERCBalanceList";
+import {getLatestBlocksWithTxs} from '../utils/getLatestBlocksWithTxs';
 export const ContextApp = React.createContext();
-
-
-
-//let web3 = new Web3('https://mainnet.telos.net/evm');
 
 
 let TLOSAddress = '0xeb29dd1ff741a8529a57cb7900e91cdc01f4f36f';
@@ -16,8 +13,24 @@ let TLOSAddress = '0xeb29dd1ff741a8529a57cb7900e91cdc01f4f36f';
 
 
 
-getERCBalanceList(TLOSAddress).then(console.log);
-getTLOSBalance(TLOSAddress).then(console.log)
+getLatestBlocksWithTxs(1000).then(console.log);
+
+// .then(blocks =>{
+//   console.log(blocks);
+//   return blocks.filter( block =>  block.transactions.length !== 0);
+
+// }).then(console.log);
+// then(data =>{
+//   let obo = data.filter(tx => tx.transactions.length !== 0);
+//   console.log(obo);
+// })
+
+ 
+
+//web3.eth.getTransactionCount(TLOSAddress).then(console.log)
+
+//getERCBalanceList(TLOSAddress).then(console.log);
+//getTLOSBalance(TLOSAddress).then(console.log)
 
 
 
---
+++ src/utils/getLatestBlocksWithTxs.js
@@ -0,0 +1,38 @@
+
+import Web3 from "web3";
+let web3 = new Web3('https://rpc1.us.telos.net/evm');
+
+export async function getLatestBlocksWithTxs (n) {
+  const getBlockNumbers = (min, max) => Array.from({ length: max - min + 1 }, (_, i) => min + i);
+  const latest = await web3.eth.getBlockNumber();
+  const limit = latest - n;
+  const blockNumbers = getBlockNumbers(limit, latest )
+  const batch = new web3.BatchRequest();
+
+  const total = blockNumbers.length;
+  let counter = 0;
+  let blocks = [];
+
+  await new Promise(function(resolve, reject){
+    blockNumbers.forEach(blockNumber => {
+      batch.add(
+        web3.eth.getBlock.request(blockNumber, true, (error, data) => {
+          if (error) return reject(error);
+
+          counter++;
+          blocks.push(data);
+          // if(data.transactions.length !== 0){
+          //   blocks.push(data);
+          // }
+          
+
+          if (counter === total) resolve();
+        })
+      )
+    });
+
+    batch.execute()
+  });
+
+  return blocks.filter( block =>  block.transactions.length !== 0);
+}
